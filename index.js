316de708d9239c3ea75edb1f36aed6bf02498c2b
mcmadhavan <mcmadhavan2001@gmail.com>
2026-01-22 16:37:28 +0530

    feat: Add CI Pipeline

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('GitHub Actions CI Pipeline Started');
+console.log('Node.js Version: ${process.version}');
+console.log('Working Directory: ${process.cwd()}');
+console.log('\n CI pipeline is ready to test this file.');
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('Starting the tests...');
+setTimeout(() => {console.log('Waiting for 3 seconds...')}, 3000); //3000ms = 3seconds
+console.log('Tests Completed!');
